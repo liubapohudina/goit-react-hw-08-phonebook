6634870497f4ae3f39a81b6872b436dccce32303
Liubov <drogaltseva92@gmail.com>
2024-04-14 12:26:55 +0200

    change route

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter  basename='/Phonebook'>
+          <BrowserRouter  basename='/Phonebook-frontend'>
             <App />
           </BrowserRouter> 
         </PersistGate>  
