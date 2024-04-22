f39a3a59a0d5c421115eab09ad70b8a950f288aa
Liubov <drogaltseva92@gmail.com>
2024-04-22 13:10:47 +0200

    change homepage

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter  basename='/Phonebook-frontend'>
+          <BrowserRouter  basename='/pohudina-phonebook.netlify.app'>
             <App />
           </BrowserRouter> 
         </PersistGate>  
